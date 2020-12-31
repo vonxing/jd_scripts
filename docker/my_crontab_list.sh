4e1d80b70c889fb85b9b0f4c36b10d278a1d328f
vonxing <284934875@qq.com>
2020-12-31 11:37:29 +0800

    更新脚本

--- docker/my_crontab_list.sh
+++ docker/my_crontab_list.sh
@@ -1,4 +1,4 @@
 # 京东发现-看一看，看40个视频领80京豆（非常耗时）
 34 17 * * * node /scripts/jd_watch.js >> /scripts/logs/jd_watch.log 2>&1
-1 9 * * * node /scripts/jd_crazy_joy.js >> /scripts/logs/jd_crazy_joy.log 2>&1
+35 9 * * * node /scripts/jd_crazy_joy.js >> /scripts/logs/jd_crazy_joy.log 2>&1
 1 */1 * * * node /scripts/jd_crazy_joy_coin.js >> /scripts/logs/jd_crazy_joy_coin.log 2>&1
